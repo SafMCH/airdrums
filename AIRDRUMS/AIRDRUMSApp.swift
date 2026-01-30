1eed43205d2145ca5e38ea5869a9dcc2cd6be8ae
safae <m692706@gmail.com>
2026-01-30 12:18:20 -0800

    Initial Commit

---
+++ AIRDRUMS/AIRDRUMSApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIRDRUMSApp.swift
+//  AIRDRUMS
+//
+//  Created by safae machmoum on 30/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct AIRDRUMSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
